8e0e26005eb480bed8472a750adc26f93fee0d85
Nwokorie Alex V <nwokoriealex20@gmail.com>
2023-10-30 17:25:02 +0100

    feat: created profile controller and updated security yaml file

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+class ProfileController extends AbstractController
+{
+    #[Route('/profile', name: 'app_profile')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function index(): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        return $this->render('profile/index.html.twig', [
+            'user' => $user,
+            'title' => 'Profile of ' . $user->getUsername()
+        ]);
+    }
+
+    #[Route('/users/all', name: 'app_all_users')]
+    public function users(UserRepository $userRepository): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_INSTRUCTOR');
+        
+        $users = $userRepository->findAll();
+        return $this->render('profile/all.html.twig', [
+            'users' => $users
+        ]);
+    }
+}
