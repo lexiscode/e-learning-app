1bba8071e24796a55c57b849dae04e3390b17bab
Nwokorie Alex V <nwokoriealex20@gmail.com>
2023-10-30 10:39:58 +0100

    fix: solved the registration mail handling error

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -49,7 +49,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('nwokoriealex20@gmail.com', 'Online Learning App'))
+                    ->from(new Address('learnlex@gmail.com', 'Registration Verification'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
@@ -80,12 +80,12 @@ class RegistrationController extends AbstractController
         } catch (VerifyEmailExceptionInterface $exception) {
             $this->addFlash('verify_email_error', $translator->trans($exception->getReason(), [], 'VerifyEmailBundle'));
 
-            return $this->redirectToRoute('app_register');
+            return $this->redirectToRoute('app_login');
         }
 
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
         $this->addFlash('success', 'Your email address has been verified.');
 
-        return $this->redirectToRoute('app_register');
+        return $this->redirectToRoute('app_profile');
     }
 }
