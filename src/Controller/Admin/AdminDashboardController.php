21746efe804ac023341ec31d6531d1a833efc592
Nwokorie Alex V <nwokoriealex20@gmail.com>
2023-11-01 12:26:05 +0100

    feat: created admin panel using symfony easyadmin package

---
+++ src/Controller/Admin/AdminDashboardController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use App\Entity\Course;
+use App\Entity\Lesson;
+use App\Entity\Enrollment;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use phpDocumentor\Reflection\DocBlock\Tags\Uses;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+
+#[IsGranted('ROLE_ADMIN')]
+class AdminDashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        //return parent::index();
+
+        // Option 1. You can make your dashboard redirect to some common page of your backend
+        //
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        return $this->redirect($adminUrlGenerator->setController(UserCrudController::class)->generateUrl());
+
+        // Option 2. You can make your dashboard redirect to different pages depending on the user
+        //
+        // if ('jane' === $this->getUser()->getUsername()) {
+        //     return $this->redirect('...');
+        // }
+
+        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
+        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+        //
+        // return $this->render('some/path/my-dashboard.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('LearnLex Admin Panel');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        return [
+
+            MenuItem::linkToDashboard('Dashboard', 'fa fa-home'),
+            MenuItem::linkToCrud('User', 'fa fa-tags', User::class),
+            MenuItem::linkToCrud('Course', 'fa fa-file-text', Course::class),
+            MenuItem::linkToCrud('Lesson', 'fa fa-user', Lesson::class),
+            MenuItem::linkToCrud('Enrollment', 'fa fa-user', Enrollment::class),
+
+        ];
+       
+        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+    }
+}
